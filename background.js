4b6dbba97cb34a80d4f018fdb182903d42ae5c62
Cameron Russell <32221303+cameron-russell@users.noreply.github.com>
2022-01-23 19:17:31 +0000

    manifest v3

--- background.js
+++ background.js
@@ -1,9 +1,15 @@
 console.log("from background");
-chrome.tabs.onActivated.addListener((tab) =>
-  chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
-    console.log("after activated")
-  )
-);
+
+// async function getCurrentTab() {
+//   let queryOptions = { active: true, currentWindow: true };
+//   let [tab] = await chrome.tabs.query(queryOptions);
+//   return tab;
+// }
+
+chrome.tabs.onActivated.addListener((tab_info) => {
+  console.log(tab_info);
+  run(tab_info);
+});
 
 // chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 //   console.log(changeInfo);
@@ -17,4 +23,12 @@ chrome.tabs.onActivated.addListener((tab) =>
 //check if page refreshed
 chrome.runtime.onMessage.addListener((message, sender, res) => {
   console.log(message);
+  run(tab_info);
 });
+
+function run(tab_info) {
+  chrome.scripting.executeScript(
+    { target: { tabId: tab_info.tabId }, files: ["./foreground.js"] },
+    () => console.log("after activated")
+  );
+}
