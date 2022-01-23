291049c5a3dfacafa578207f5163197d032a6481
Cameron Russell <32221303+cameron-russell@users.noreply.github.com>
2022-01-23 20:38:09 +0000

    front call

--- foreground.js
+++ foreground.js
@@ -1,2 +1,8 @@
 console.log("executing foreground script");
+const el = document.querySelector(
+  ".details-reset.details-overlay.dropdown.float-right.mt-1"
+);
+console.log(el);
+// const html = fetchrome.runtime.getURL("./popup.html").then((r) => r.text());
+// el.insertAdjacentHTML("beforebegin", html);
 // chrome.scripting.insertCSS({ css: "" }, () => console.log("css injected!"));
