cc2ca605ab390c788bf38bed10b45cdac3c08f99
Cameron Russell <32221303+cameron-russell@users.noreply.github.com>
2022-01-24 19:42:12 +0000

    styling

--- foreground.js
+++ foreground.js
@@ -2,14 +2,28 @@ console.log("executing foreground script");
 // let el = document.querySelector(
 //   ".details-reset.details-overlay.dropdown.float-right.mt-1"
 // );
-let menu = `<details-menu id="gcccc-color-menu" class="gcccc-closed contributions-setting-menu" role="menu">
+let menu = `<details-menu id="gcccc-color-menu" class="gcccc-closed" role="menu">
   <form class="edit_user" accept-charset="UTF-8" method="post">
-    
+  <div id="gcccc-lessmore" class="color-fg-muted">
+  Less
+    <svg width="10" height="10" class="d-inline-block">
+      <rect width="10" height="10" class="ContributionCalendar-day" rx="2" ry="2" data-level="0"></rect>
+    </svg>
+    <svg width="10" height="10" class="d-inline-block">
+      <rect width="10" height="10" class="ContributionCalendar-day" rx="2" ry="2" data-level="1"></rect>
+    </svg>
+    <svg width="10" height="10" class="d-inline-block">
+      <rect width="10" height="10" class="ContributionCalendar-day" rx="2" ry="2" data-level="2"></rect>
+    </svg>
+    <svg width="10" height="10" class="d-inline-block">
+      <rect width="10" height="10" class="ContributionCalendar-day" rx="2" ry="2" data-level="3"></rect>
+    </svg>
+    <svg width="10" height="10" class="d-inline-block">
+      <rect width="10" height="10" class="ContributionCalendar-day" rx="2" ry="2" data-level="4"></rect>
+    </svg>
+  More
+</div>
   </form>    
-  <div role="none" class="dropdown-divider"></div>
-  <form class="edit_user" action="/users/cameron-russell/set_activity_overview_preference" accept-charset="UTF-8" method="post">
-    
-  </form>
 </details-menu>`;
 
 let el = document.querySelector(".js-yearly-contributions");
@@ -23,8 +37,8 @@ el.insertAdjacentElement("afterend", div);
 div.insertAdjacentHTML("beforeend", menu);
 // chrome.scripting.insertCSS({ css: "" }, () => console.log("css injected!"));
 let gccccMenu = document.getElementById("gcccc-color-menu");
-div.addEventListener("click", () => toggleMenu(gccccMenu.classList));
 
+div.addEventListener("click", () => toggleMenu(gccccMenu.classList));
 function toggleMenu(classList) {
   if (classList.contains("gcccc-closed")) {
     gccccMenu.classList.remove("gcccc-closed");
